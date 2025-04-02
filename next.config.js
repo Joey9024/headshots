640b41589526423fe2647856c2a40a2daccdcbd6
Joey9024 <zhaoxig1990@gmail.com>
2025-04-02 19:09:03 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    esmExternals: "loose",
-  },
-};
+    missingSuspenseWithCSRError: false
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
